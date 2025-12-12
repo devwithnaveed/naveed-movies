382cac9de53e1e36a431e99f9186ee73cf8397ea
devwithnaveed <786muhammadnaveed@gmail.com>
2025-12-12 21:08:51 +0500

    Professor Suggestions

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,32 +1,22 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
-import React, {useReducer} from "react";
-
-const initialState = {
-  email: "",
-  password: "",
-  isShowPassword: false,
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "SET_EMAIL":
-      return { ...state, email: action.payload };
-    case "SET_PASSWORD":
-      return { ...state, password: action.payload };
-    case "SET_SHOW_PASSWORD":
-      return { ...state, isShowPassword: !state.isShowPassword };
-    default:
-      return state;
-  }
-}
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
 
 const LoginForm = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    showPassword: false,
+  });
 
-  const handlePasswordCToggle = (e) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const togglePassword = (e) => {
     e.preventDefault();
-    dispatch({ type: "SET_SHOW_PASSWORD" });
+    setFormData({ ...formData, showPassword: !formData.showPassword });
   };
 
   const handleSubmit = (e) => {
@@ -39,29 +29,27 @@ const LoginForm = () => {
         <label>Email</label>
         <input
           type="email"
+          name="email"
           placeholder="Enter email"
-          value={state.email}
-          onChange={(e) =>
-            dispatch({ type: "SET_EMAIL", payload: e.target.value })
-          }
+          value={formData.email}
+          onChange={handleChange}
         />
       </div>
 
       <div className="form-group">
         <label>Password</label>
         <input
-          type={state.isShowPassword ? "text" : "password"}
+          type={formData.showPassword ? "text" : "password"}
+          name="password"
           placeholder="Enter password"
-          value={state.password}
-          onChange={(e) =>
-            dispatch({ type: "SET_PASSWORD", payload: e.target.value })
-          }
+          value={formData.password}
+          onChange={handleChange}
         />
-        <span onClick={handlePasswordCToggle} style={{ cursor: "pointer" }}>
-          {state.isShowPassword ? (
-            <FontAwesomeIcon icon={faEyeSlash} className="customIcon"/>
+        <span onClick={togglePassword} style={{ cursor: "pointer" }}>
+          {formData.showPassword ? (
+            <FontAwesomeIcon icon={faEyeSlash} className="customIcon" />
           ) : (
-            <FontAwesomeIcon icon={faEye} className="customIcon"/>
+            <FontAwesomeIcon icon={faEye} className="customIcon" />
           )}
         </span>
       </div>
--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,35 +1,23 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEye, faEyeSlash} from "@fortawesome/free-solid-svg-icons";
-import React, {useReducer} from "react";
-
-const initialState = {
-  userName: "",
-  email: "",
-  password: "",
-  isShowPassword: false,
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "SET_USER_NAME":
-      return { ...state, userName: action.payload };
-    case "SET_EMAIL":
-      return { ...state, email: action.payload };
-    case "SET_PASSWORD":
-      return { ...state, password: action.payload };
-    case "SET_SHOW_PASSWORD":
-      return { ...state, isShowPassword: !state.isShowPassword };
-    default:
-      return state;
-  }
-}
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
 
 const RegistrationForm = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [formData, setFormData] = useState({
+    userName: "",
+    email: "",
+    password: "",
+    showPassword: false,
+  });
 
-  const handlePasswordCToggle = (e) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const togglePassword = (e) => {
     e.preventDefault();
-    dispatch({ type: "SET_SHOW_PASSWORD" });
+    setFormData({ ...formData, showPassword: !formData.showPassword });
   };
 
   const handleSubmit = (e) => {
@@ -42,11 +30,10 @@ const RegistrationForm = () => {
         <label>User Name</label>
         <input
           type="text"
+          name="userName"
           placeholder="Enter username"
-          value={state.userName}
-          onChange={(e) =>
-            dispatch({ type: "SET_USER_NAME", payload: e.target.value })
-          }
+          value={formData.userName}
+          onChange={handleChange}
         />
       </div>
 
@@ -54,29 +41,27 @@ const RegistrationForm = () => {
         <label>Email</label>
         <input
           type="email"
+          name="email"
           placeholder="Enter email"
-          value={state.email}
-          onChange={(e) =>
-            dispatch({ type: "SET_EMAIL", payload: e.target.value })
-          }
+          value={formData.email}
+          onChange={handleChange}
         />
       </div>
 
       <div className="form-group">
         <label>Password</label>
         <input
-          type={state.isShowPassword ? "text" : "password"}
+          type={formData.showPassword ? "text" : "password"}
+          name="password"
           placeholder="Enter password"
-          value={state.password}
-          onChange={(e) =>
-            dispatch({ type: "SET_PASSWORD", payload: e.target.value })
-          }
+          value={formData.password}
+          onChange={handleChange}
         />
-        <span onClick={handlePasswordCToggle} style={{ cursor: "pointer" }}>
-          {state.isShowPassword ? (
-            <FontAwesomeIcon icon={faEyeSlash} className="customIcon"/>
+        <span onClick={togglePassword} style={{ cursor: "pointer" }}>
+          {formData.showPassword ? (
+            <FontAwesomeIcon icon={faEyeSlash} className="customIcon" />
           ) : (
-            <FontAwesomeIcon icon={faEye} className="customIcon"/>
+            <FontAwesomeIcon icon={faEye} className="customIcon" />
           )}
         </span>
       </div>
